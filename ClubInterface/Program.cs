5e414216d856619f3b337ec772e8f9ad7174b9d4
kayde <kayde@KAYDENS-PC>
2024-03-06 11:20:58 -0500

    Add project files.

---
+++ ClubInterface/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+interface IClub
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public string Fullname();
+}
+
+class ClubMember : IClub
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string AdditionalInfo { get; set; }
+    public DateTime JoinDate { get; set; }
+
+    public ClubMember()
+    {
+        Id = 0;
+        FirstName = "Kayden";
+        LastName = "Krumrie";
+    }
+
+    public ClubMember(int id, string firstName, string lastName, string email, string additionalInfo, DateTime joinDate)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        AdditionalInfo = additionalInfo;
+        JoinDate = joinDate;
+    }
+
+    public void DisplayInformation()
+    {
+        Console.WriteLine($"ID: {Id}");
+        Console.WriteLine($"Full Name: {Fullname()}");
+        Console.WriteLine($"Email: {Email}");
+        Console.WriteLine($"Additional Info: {AdditionalInfo}");
+        Console.WriteLine($"Join Date: {JoinDate}");
+    }
+    public string Fullname()
+    {
+        return $"{FirstName} {LastName}";
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        ClubMember member = new ClubMember(1, "Kayden", "Krumrie", "krumrik@mail.nmc.edu", "President of the Club", DateTime.Now);
+
+        member.DisplayInformation();
+    }
+}
+
